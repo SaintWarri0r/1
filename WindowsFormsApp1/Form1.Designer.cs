6a91ed38adb9b990f554d4156d8f76a7ef1a74ba
SaintWarrior <SaintWarrior@DESKTOP-EBH6G4L>
2022-06-01 22:38:04 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(513, 115);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "button1";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(32, 53);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(362, 20);
+            this.tb1.TabIndex = 1;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(32, 159);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(362, 20);
+            this.tb2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            String st;
+            
+            st = tb1.Text.ToString();
+            char[] a = st.ToCharArray();
+            char[] b = new char[a.Length];
+            char letter = a[1];
+            for (int i = 0; i < a.Length; i++)
+            {
+                int counter = 0;
+                letter = a[i];
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (a[j] == letter)
+                    {
+                        counter += 1;
+                    }
+                
+                        
+                }
+                
+                if (counter <= 1)
+                {
+                    b[i] = a[i];
+                }
+                else if(counter > 1)
+                {
+                    b[i] = ' ';
+                }
+                
+                counter = 0;
+
+            }
+
+            string str2 = new string(b);
+            tb2.Text = str2;
+
+        }
+    }
+}
